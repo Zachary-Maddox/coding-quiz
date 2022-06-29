6cd80f8850692577ecc5959e43897c92e5da09a9
Zachary-Maddox <z19s96p@aol.com>
2022-06-28 22:49:01 -0500

    intergrated bootstrap,added javascript code,created a timer that counts down and subtracts 5 if you are incorrect,made a highscore list that save locally,need to add alerts for answers and prettify the page.

---
+++ script.js
@@ -0,0 +1,129 @@
+var questions = [
+    {
+        prompt: "Which answer is not a primitve data type?",
+        choices: ["String", "Boolean", "Number", "Tom Brady"],
+        answer: "Tom Brady",
+    },
+    {
+        prompt: "Hoisting is the default behaviour of javascript where all the variable and function declarations are moved on top.",
+        choices: ["True", "False", "Maybe", "Bananas"],
+        answer: "True",
+    },
+    {
+        prompt: " What does === mean?",
+        choices: [
+            "Image of a road",
+            "Equal in value",
+            "Space filler",
+            "Equal in value and data type",
+        ],
+        answer: "Equal in value and data type",
+    },
+    {
+        prompt: "Is javascript a statically typed or a dynamically typed language?",
+        choices: [
+            "User typed",
+            "Statically typed",
+            "Dynamically typed",
+            "None of the above",
+        ],
+        answer: "Dynamically typed",
+    },
+    {
+        prompt: "Callback is a function that will be executed after another function gets executed.",
+        choices: ["True", "False", "Toaster", "None of the above"],
+        answer: "True",
+    },
+];
+const startQuizEl = document.querySelector(".startQuiz");
+const beginQuizEl = document.querySelector(".beginQuiz");
+const questionContainerEl = document.querySelector(".questionContainer");
+let currentQuestion = 0;
+const answerBtnsEl = document.querySelectorAll(".answerBtn");
+const questionEl = document.querySelector(".question");
+const topRankedEl = document.querySelector(".topRanked");
+let timer = 60;
+const timerEl = document.querySelector(".timer");
+let clock;
+const buttonAddon2El = document.querySelector("#buttonAddon2");
+const highScores = [];
+
+const initialsEl = document.querySelector(".initials");
+const initialsInputEl = document.querySelector(".initialsInput");
+const listGroupEl = document.querySelector(".listGroup");
+const theBestEl = document.querySelector(".theBest");
+
+function saveHighScore() {
+    console.log(initialsEl.value);
+    const scoreObj = {
+        initials: initialsEl.value,
+        score: timer,
+    };
+    highScores.push(scoreObj);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    initialsInputEl.classList.add("d-none");
+
+    const highScoresStorage = JSON.parse(localStorage.getItem("highScores"));
+    console.log(highScoresStorage);
+    theBestEl.classList.remove("d-none");
+    highScoresStorage.forEach((item) => {
+        const newLi = document.createElement("li");
+        newLi.innerText = `${item.initials}:${item.score}`;
+        newLi.classList.add("list-group-item");
+        listGroupEl.appendChild(newLi);
+    });
+}
+
+function showNextQuestion() {
+    currentQuestion++;
+    questionEl.innerText = questions[currentQuestion].prompt;
+    answerBtnsEl.forEach(
+        (button, i) =>
+            (button.innerText = questions[currentQuestion].choices[i])
+    );
+}
+function endQuiz() {
+    clearInterval(clock);
+    questionContainerEl.classList.add("d-none");
+    topRankedEl.classList.remove("d-none");
+    console.log("Thanks for playing!");
+}
+
+function checkAnswer(e) {
+    console.log(currentQuestion);
+    if (questions[currentQuestion].answer == e.target.innerText) {
+        if (currentQuestion < questions.length - 1) {
+            showNextQuestion();
+        } else {
+            endQuiz();
+        }
+        console.log("correctAnswer");
+    } else {
+        timer = timer - 5;
+        if (currentQuestion < questions.length - 1) {
+            showNextQuestion();
+        } else {
+            endQuiz();
+        }
+        console.log("wrongAnswer");
+    }
+}
+
+function startTest() {
+    clock = setInterval(() => {
+        timer--;
+        if (timer <= 0) {
+            endQuiz();
+        }
+        timerEl.innerText = timer;
+    }, 1000);
+
+    startQuizEl.classList.add("d-none");
+    questionContainerEl.classList.remove("d-none");
+    answerBtnsEl.forEach((button) =>
+        button.addEventListener("click", checkAnswer)
+    );
+}
+
+beginQuizEl.addEventListener("click", startTest);
+buttonAddon2El.addEventListener("click", saveHighScore);
